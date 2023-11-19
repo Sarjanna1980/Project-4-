86ef4397fa1c0cdc8da051c518d1d01d012fa10a
Sahar <sarjanna@gmail.com>
2023-11-19 10:12:45 +0200

    first

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import openpyxl
 from openpyxl import load_workbook
 from openpyxl import load_workbook
 
-def copy_values_between_files(source_file, source_sheet, start_row, end_row, source_column, target_file, target_sheet, target_column):
+def copy_values_v1_between_files(source_file, source_sheet, start_row, end_row, source_column, target_file, target_sheet, target_column):
     source_workbook = load_workbook(source_file)
     source_sheet = source_workbook[source_sheet]
 
@@ -12,9 +12,12 @@ def copy_values_between_files(source_file, source_sheet, start_row, end_row, sou
     target_row = 27  # Start copying to P27
 
     for row_index in range(start_row, end_row + 1, step):
-        source_cell = source_sheet.cell(row=row_index, column=source_column)
-        target_cell = target_sheet.cell(row=target_row, column=target_column)
-        target_cell.value = source_cell.value
+        source_cell_v1 = source_sheet.cell(row=row_index, column=source_column_v1)
+        target_cell_v1 = target_sheet.cell(row=target_row, column=target_column_v1)
+        source_cell_v2 = source_sheet.cell(row=row_index, column=source_column_v2)
+        target_cell_v2 = target_sheet.cell(row=target_row, column=target_column_v2)
+        target_cell_v1.value = source_cell_v1.value
+        target_cell_v2.value = source_cell_v2.value
 
         target_row += 1
 
@@ -22,18 +25,21 @@ def copy_values_between_files(source_file, source_sheet, start_row, end_row, sou
     source_workbook.close()
     target_workbook.close()
 
+
+
 # Example usage:
 source_file ="C:/Users/Sahar/Desktop/leonid_test/Test data.xlsx"
 source_sheet = "sheet2"
-start_row = 5
+start_row = 6
 end_row = 28
-source_column = 3  # Column C
+source_column_v1 = 3  # Column C
+source_column_v2 = 4
 step = 2
 target_file ="C:/Users/Sahar/Desktop/leonid_test/UpdatedFormatForDCIRRPT.xlsx"
 target_sheet = "Sheet1"
-target_column = 16  # Column P
-print(target_sheet)
-copy_values_between_files(source_file, source_sheet, start_row, end_row, source_column, target_file, target_sheet, target_column)
+target_column_v1 = 16  # Column P
+target_column_v2= 17
+copy_values_v1_between_files(source_file, source_sheet, start_row, end_row, source_column_v1, target_file, target_sheet, target_column_v1)
 
 
 
